a93f1eee47780781d32b8ae0ff5129c7f1469b95
Chris Hannam <bassdread@gmail.com>
2011-05-04 10:29:07 +0100

    added config for debugging

--- src/chishop/chishop/settings.py
+++ src/chishop/chishop/settings.py
@@ -19,3 +19,35 @@ DATABASES = {
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
 HAYSTACK_SITECONF = 'chishop.search_sites'
 HAYSTACK_WHOOSH_PATH = os.path.join(os.path.dirname(chishop.__file__), 'haystack')
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'log_file':{
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(os.path.abspath(os.path.dirname(chishop.__file__)), 'logs/django.log'),
+            'maxBytes': '16777216', # 16megabytes
+            'formatter': 'verbose',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'djangopypi.views.distutils': {
+            'handlers': ['log_file'],
+            'level': 'INFO'
+        }
+    }
+} 
