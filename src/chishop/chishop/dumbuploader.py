9a23e858e7bf5fb51e66f5de172f0d4379919bc3
Gary <gjm@isotoma.com>
2011-05-09 18:40:19 +0100

    adding command line handling for extra demonstration of how to use

--- src/chishop/chishop/dumbuploader.py
+++ src/chishop/chishop/dumbuploader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from distutils.command.register import register
 from distutils.command.upload import upload
 from distutils.core import Distribution
@@ -12,7 +14,7 @@ DEF_FIELDS = {'version':'0.0.1',
               'url':'http://unknown/'}
 
 class DumbUploader(object):
-    
+    '''Uploader for random bdist files'''
     def __init__(self, repository, username, password):
         self.repository = repository
         self.username = username
@@ -23,7 +25,11 @@ class DumbUploader(object):
     
     def setDistFile(self,filename):
         self.filename = filename
-        package = BDist(filename)
+        try:
+            package = BDist(filename)
+        except ValueError, e:
+            print e
+            return False
         
         metadata = dict([(k,getattr(package,k)) for k in package.iterkeys()
                          if getattr(package,k)])
@@ -45,16 +51,19 @@ class DumbUploader(object):
                 metadata[k] = v
         
         self.distribution = Distribution(metadata)
+        return True
     
     def register(self):
         #python setup.py register -r chishop --show-response
         r = register(self.distribution)
         r.repository = self.repository
         r.run()
+        print '%s registered' %(self.filename)
     
     def bdist_dumb(self):
         #python setup.py sdist upload -r http://localhost:8000/ --show-response
         command = 'bdist_dumb'
+        
         pyversion = 'any'
         self.distribution.dist_files.append((command,pyversion,self.filename))
         u = upload(self.distribution)
@@ -62,14 +71,94 @@ class DumbUploader(object):
         u.password = self.password
         u.repository = self.repository
         u.run()
+        print '%s uploaded' %(self.filename)
+
     
 if __name__ == '__main__':
-    repository = 'http://localhost:8000'
-    username = 'admin'
-    password = 'password'
-    filename = '/home/user/test.egg'
+    from optparse import OptionParser
+    from ConfigParser import ConfigParser
+    import os.path
     
-    uploader = DumbUploader(repository,username,password)
-    uploader.setDistFile(filename)
-    uploader.register()
-    uploader.bdist_dumb()
+    def read_rc_file(f,repository):
+        '''stolen from distutils.config'''
+        if os.path.exists(f):
+            config = ConfigParser()
+            config.read(f)
+            sections = config.sections()
+            if 'distutils' in sections:
+                index_servers = config.get('distutils', 'index-servers')
+                _servers = [server.strip() for server in
+                            index_servers.split('\n')
+                            if server.strip() != '']
+                if _servers == []:
+                    # nothing set, let's try to get the default pypi
+                    if 'pypi' in sections:
+                        _servers = ['pypi']
+                    else:
+                        # the file is not properly defined, returning
+                        # an empty dict
+                        return {}
+                for server in _servers:
+                    current = {'server': server}
+                    current['username'] = config.get(server, 'username')
+                    current['password'] = config.get(server, 'password')
+                    
+                    # optional params
+                    if config.has_option(server,'repository'):
+                        current['repository'] = config.get(server,'repository')
+                    else:
+                        current['repository'] = repository
+                    
+                    if (current['server'] == repository or 
+                        current['repository'] == repository):
+                        return current
+            elif 'server-login' in sections:
+                # old format
+                server = 'server-login'
+                if config.has_option(server, 'repository'):
+                    repo = config.get(server, 'repository')
+                else:
+                    repo = repository
+                return {'username':config.get(server,'username'),
+                        'password':config.get(server,'password'),
+                        'repository':repo,
+                        'server':server}
+        return {}
+    
+    usage = 'usage: %prog [options] arg [arg1 [arg2 ...]]'
+    parser = OptionParser(usage=usage)
+    parser.add_option('-r', '--repository',dest='repository',
+                      default='http://localhost:8000/',
+                      help='upload to REPOSITORY', metavar='REPOSITORY')
+    parser.add_option('-c', '--config-file',dest='rcfile',
+                      default=os.path.join(os.path.expanduser('~'),'.pypirc'),
+                      help='full path for pypi config file')
+    
+    
+    (options, args) = parser.parse_args()
+    if not args:
+        print '''No files to upload
+'''
+        parser.print_help()
+        parser.exit(1)
+        
+    config = read_rc_file(options.rcfile,options.repository)
+    if config:
+        repository = config['repository']
+        username = config['username']
+        password = config['password']
+    else:
+        print '''Could not find username and password details.
+Check your pypi config file has these details and you are selecting the matching repository
+'''
+        parser.print_help()
+        parser.exit(2)
+    
+    uploader = DumbUploader(repository,
+                            username,
+                            password)
+    for filename in args:
+        if os.path.exists(filename):
+            if uploader.setDistFile(filename):
+                uploader.register()
+                uploader.bdist_dumb()
