92652f07960784b4ff8898f14facf4f593187ddb
Chris Hannam <bassdread@gmail.com>
2011-05-11 09:14:35 +0100

    minor style fixes to make pylint happy

--- src/chishop/chishop/uploaders/smartuploader.py
+++ src/chishop/chishop/uploaders/smartuploader.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+"""Give this script the path to uploaded python libs and it will upload to
+a egg server."""
+
 from distutils.command.register import register
 from distutils.command.upload import upload
 from distutils.core import Distribution
@@ -26,7 +29,8 @@ class DumbUploader(object):
         self.filename = None
         self.distribution = None
     
-    def setDistFile(self,filename):
+    def set_dist_file(self, filename):
+        """Setup our distribution using details from the binary"""
         self.filename = filename
         try:
             package = BDist(filename)
@@ -34,22 +38,22 @@ class DumbUploader(object):
             print e
             return False
         
-        metadata = dict([(k,getattr(package,k)) for k in package.iterkeys()
-                         if getattr(package,k)])
+        metadata = dict([(k, getattr(package, k)) for k in package.iterkeys()
+                         if getattr(package, k)])
         
-        for (inkey,outkey) in FIELD_TRANSLATE.iteritems():
-            if getattr(package,inkey,None):
+        for (inkey, outkey) in FIELD_TRANSLATE.iteritems():
+            if getattr(package, inkey, None):
                 metadata[outkey] = metadata[inkey]
                 del metadata[inkey]
         
         got_a = 'author' in metadata and 'author_email' in metadata
         got_m = 'maintainer' in metadata and 'maintainer_email' in metadata
         if not(got_a or got_m):
-            for (k,v) in DEF_AUTHOR.iteritems():
+            for (k, v) in DEF_AUTHOR.iteritems():
                 if k not in metadata:
                     metadata[k] = v
         
-        for (k,v) in DEF_FIELDS.iteritems():
+        for (k, v) in DEF_FIELDS.iteritems():
             if k not in metadata:
                 metadata[k] = v
         
@@ -57,36 +61,36 @@ class DumbUploader(object):
         return True
     
     def register(self):
-        #python setup.py register -r chishop --show-response
+        """Register a distribution code version of:
+        python setup.py register -r chishop --show-response
+        """
         r = register(self.distribution)
         r.repository = self.repository
         r.run()
-        print '%s registered' %(self.filename)
+        print '%s registered' % (self.filename)
     
     def bdist_dumb(self):
-        #python setup.py sdist upload -r http://localhost:8000/ --show-response
+        """Upload a binary distribution using the code version of:
+        python setup.py sdist upload -r http://localhost:8000/ --show-response
+        """
         command = 'bdist_dumb'
         
         pyversion = 'any'
-        self.distribution.dist_files.append((command,pyversion,self.filename))
-        u = upload(self.distribution)
-        u.username = self.username
-        u.password = self.password
-        u.repository = self.repository
-        u.run()
-        print '%s uploaded' %(self.filename)
+        self.distribution.dist_files.append((command, pyversion, self.filename))
+        distribution_upload = upload(self.distribution)
+        distribution_upload.username = self.username
+        distribution_upload.password = self.password
+        distribution_upload.repository = self.repository
+        distribution_upload.run()
+        print '%s uploaded' % (self.filename)
 
 def unpack(filename):
     """Check what sort of package we have (tar.gz, zip or egg), unpack it and 
     get it uploaded."""
 
     if filename.endswith('.tar.gz') or filename.endswith('.zip'):
-        print 'tar/zip!'
         if decompress(filename):
             return True
-
-def crack(filename):
-    """Crack (heh) open the egg and get the meta data out ready for upload."""
 
 def decompress(filename):
     """Take either a zip or gz file and decompress it and untar it if needed."""    
@@ -135,14 +139,14 @@ if __name__ == '__main__':
     from ConfigParser import ConfigParser
     import os.path
     
-    def read_rc_file(f,repository):
+    def read_rc_file(f, repository):
         '''stolen from distutils.config'''
         if os.path.exists(f):
-            config = ConfigParser()
-            config.read(f)
-            sections = config.sections()
+            config_parser = ConfigParser()
+            config_parser.read(f)
+            sections = config_parser.sections()
             if 'distutils' in sections:
-                index_servers = config.get('distutils', 'index-servers')
+                index_servers = config_parser.get('distutils', 'index-servers')
                 _servers = [server.strip() for server in
                             index_servers.split('\n')
                             if server.strip() != '']
@@ -156,12 +160,12 @@ if __name__ == '__main__':
                         return {}
                 for server in _servers:
                     current = {'server': server}
-                    current['username'] = config.get(server, 'username')
-                    current['password'] = config.get(server, 'password')
+                    current['username'] = config_parser.get(server, 'username')
+                    current['password'] = config_parser.get(server, 'password')
                     
                     # optional params
-                    if config.has_option(server,'repository'):
-                        current['repository'] = config.get(server,'repository')
+                    if config_parser.has_option(server,'repository'):
+                        current['repository'] = config_parser.get(server,'repository')
                     else:
                         current['repository'] = repository
                     
@@ -171,8 +175,8 @@ if __name__ == '__main__':
             elif 'server-login' in sections:
                 # old format
                 server = 'server-login'
-                if config.has_option(server, 'repository'):
-                    repo = config.get(server, 'repository')
+                if config_parser.has_option(server, 'repository'):
+                    repo = config_parser.get(server, 'repository')
                 else:
                     repo = repository
                 return {'username':config.get(server,'username'),
@@ -183,10 +187,10 @@ if __name__ == '__main__':
     
     usage = 'usage: %prog [options] arg [arg1 [arg2 ...]]'
     parser = OptionParser(usage=usage)
-    parser.add_option('-r', '--repository',dest='repository',
+    parser.add_option('-r', '--repository', dest='repository',
                       default='http://localhost:8000/',
                       help='upload to REPOSITORY', metavar='REPOSITORY')
-    parser.add_option('-c', '--config-file',dest='rcfile',
+    parser.add_option('-c', '--config-file', dest='rcfile',
                       default=os.path.join(os.path.expanduser('~'),'.pypirc'),
                       help='full path for pypi config file')
     
@@ -205,7 +209,7 @@ if __name__ == '__main__':
         parser.exit(1)
         
         
-    config = read_rc_file(options.rcfile,options.repository)
+    config = read_rc_file(options.rcfile, options.repository)
     if config:
         repository = config['repository']
         username = config['username']
@@ -227,7 +231,7 @@ Check your pypi config file has these details and you are selecting the matching
         print filename
         if not filename.endswith('version_cache'):
             if not filename.endswith('egg'):
-                dir = unpack(filename)
+                unpack(filename)
                 if filename.endswith('.zip'):
                     files_dir = filename.rsplit('.', 1)[0]
                 elif filename.endswith('.gz'):
@@ -243,7 +247,7 @@ Check your pypi config file has these details and you are selecting the matching
                 
             elif filename.endswith('egg'):
                 if os.path.exists(filename):
-                    if uploader.setDistFile(filename):
+                    if uploader.set_dist_file(filename):
                         uploader.register()
                         uploader.bdist_dumb()
         chdir(args[0])
