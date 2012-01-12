478720c34a31d14ac91b1fd987fd41d9728dadfd
Alex Holmes <alex@alex-holmes.com>
2012-01-12 16:58:13 +0000

    Prepare for release 0.1.1-isotoma16

--- src/chishop/setup.py
+++ src/chishop/setup.py
@@ -22,7 +22,7 @@ tests_require = [
 
 setup(
     name='chishop',
-    version="0.1.1-isotoma15",
+    version="0.1.1-isotoma16",
     description='Simple PyPI server written in Django.',
     author='Ask Solem',
     author_email='askh@opera.com',
